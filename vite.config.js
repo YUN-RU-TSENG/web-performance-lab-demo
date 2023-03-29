138a57f84a658bde1834968029b4cbd2ba9d35b1
YUN-RU-TSENG <coco002197@gmail.com>
2023-03-29 11:49:01 +0800

    refactor: clear console and debugger on production

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((config) => ({
     plugins: [vue()],
     // 關閉使用 ESBuild minify CSS，改使用 postCSS cssnano，設置見 postcss.config.cjs
     build: {
         cssMinify: false,
     },
-})
+    // build production 時刪除 console、debugger
+    esbuild: {
+        drop: config.mode === "production" ? ["console", "debugger"] : [],
+    },
+}))
