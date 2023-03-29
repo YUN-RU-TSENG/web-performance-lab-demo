7179de6d34ec40a901578c6e47593cc45903b454
YUN-RU-TSENG <coco002197@gmail.com>
2023-03-29 11:50:59 +0800

    chore: initial unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig((config) => ({
-    plugins: [vue()],
+    plugins: [vue(), Components({})],
     // 關閉使用 ESBuild minify CSS，改使用 postCSS cssnano，設置見 postcss.config.cjs
     build: {
         cssMinify: false,
