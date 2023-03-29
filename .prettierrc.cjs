deebcab24e377af4277b1e060fa422f6be3799d6
YUN-RU-TSENG <coco002197@gmail.com>
2023-03-29 11:08:14 +0800

    chore: add editorconfig file

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
     semi: false, // Print semicolons at the ends of statements. false -> don't print
     vueIndentScriptAndStyle: true, // Whether or not to indent the code inside <script> and <style> tags in Vue files.
-    tabWidth: 4, // Specify the number of spaces per indentation-level.
 }
