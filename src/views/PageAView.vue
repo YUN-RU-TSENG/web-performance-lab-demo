f97e1db8f9690dc38dc2fd8f9ecead08fef70593
YUN-RU-TSENG <coco002197@gmail.com>
2023-04-25 15:02:28 +0800

    feat: add big lodash

--- src/views/PageAView.vue
+++ src/views/PageAView.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+    import { ref } from "vue"
+    import _ from "lodash"
+
+    const testData = ref([1, 2, 3, 4, 5, 6, 7])
+
+    function reverseTestData() {
+        const data = testData.value.slice()
+        testData.value = _.reverse(data)
+    }
+</script>
 
 <template>
-    <div>PageA</div>
+    <div>
+        <h2>PageA</h2>
+        <section>
+            <button @click="reverseTestData">reverse by lodash</button>
+            <ul>
+                <li v-for="item of testData" :key="item">{{ item }}</li>
+            </ul>
+        </section>
+    </div>
 </template>
 
 <style scoped></style>
