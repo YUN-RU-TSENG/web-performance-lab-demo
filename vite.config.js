f31d9af048d6ef2416f33620f9bb2898fc37198a
YUN-RU-TSENG <coco002197@gmail.com>
2023-04-26 15:56:14 +0800

    chore: rollup-plugin-visualizer setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite"
+import { visualizer } from "rollup-plugin-visualizer"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
 import Components from "unplugin-vue-components/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig((config) => ({
-    plugins: [vue(), Components({})],
+    plugins: [vue(), Components({}), visualizer()],
     // 關閉使用 ESBuild minify CSS，改使用 postCSS cssnano，設置見 postcss.config.cjs
     build: {
         cssMinify: false,
